9a88dbeb2f700b478d17579aa8d36c4b3ade92f1
Ritwik Pandey <ritwikmni@gmail.com>
2026-03-03 23:39:14 +0530

    Add finder

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import Navbar from "#components/Navbar";
 import Welcome from "#components/Welcome";
 import Dock from '#components/Dock'
 import Terminal from "#windows/Terminal";
+import Safari from "#windows/Safari";
+import Resume from "#windows/Resume";
+import Finder from "#windows/Finder";
 import gsap from "gsap";
 import { Draggable } from "gsap/Draggable";
 gsap.registerPlugin(Draggable);
@@ -13,6 +16,9 @@ const App = () => {
       <Welcome/>
       <Dock/>
       <Terminal/>
+      <Safari/>
+      <Resume/>
+      <Finder/>
     </main>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,19 @@
 import { navIcons, navLinks } from "#constants";
+import useWindowStore from "#store/window";
 import dayjs from "dayjs";
 import React from "react";
 
 const Navbar = () => {
+    const {openWindow} = useWindowStore();
+
     return (
         <nav>
             <div>
                 <img src="/images/logo.svg" alt="logo" />
                 <p className="font-bold">Ritwik's Portfolio</p>
                 <ul>
-                    {navLinks.map(({id, name}) => (
-                        <li key={id}>
+                    {navLinks.map(({id, name, type}) => (
+                        <li key={id} onClick={() => openWindow(type)}>
                             <p>{name}</p>
                         </li>
                     ))}
